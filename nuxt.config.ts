13c67853d54d76b0ff07b53707fe85c9442b576b
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-10 22:39:07 +0700

    add vite extend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  vite: {
+    server: {
+      fs: {
+        allow: [".."],
+      },
+    },
+  },
+});
