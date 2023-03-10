8229c93190e1a1d01e2de97ddd8442013b1c768d
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-10 22:50:32 +0700

    add tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,10 @@ import Dashboard from 'tbb-ui/src/components/layouts/Dashboard.vue'
 </script>
 
 <template>
-    <div>HELLO</div>
+    <Dashboard>
+        <div
+            class="bg-red flex justify-between items-stretch px-3 mx-auto w-full leading-5 text-left text-gray-700 lg:px-6 lg:py-0">
+        </div>
+
+    </Dashboard>
 </template>
