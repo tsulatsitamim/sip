0085e990cc68c90a47a23a4eb68d1ae0e0991509
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-11 22:59:11 +0700

    change logo

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,21 @@
 import Dashboard from 'tbb-ui/src/components/layouts/Dashboard.vue'
 import AppNavbar from 'tbb-ui/src/components/layouts/AppNavbar.vue'
 import AppSidebar from 'tbb-ui/src/components/layouts/AppSidebar.vue'
+
+const setting = {
+    title: 'Mahad Ar-Ridho'
+}
+
+const menus = [
+    { name: 'Santri', path: '/santri' },
+    { name: 'Kelas', path: '/kelas' },
+]
 </script>
 
 <template>
     <div>
-        <AppSidebar></AppSidebar>
-        <AppNavbar></AppNavbar>
+        <AppSidebar :title="setting.title" :menus="menus"></AppSidebar>
+        <AppNavbar :title="setting.title"></AppNavbar>
         <div id="content" class="h-screen bg-red-500">
             hello
         </div>
