26817289cf44f9e36664a2a64a3afed606e5a919
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-12 09:35:45 +0700

    add datatable

--- pages/kelas.vue
+++ pages/kelas.vue
@@ -1,8 +1,23 @@
-<script setup>
+<script setup lang="ts">
+import DataTable from 'tbb-ui/src/components/table/DataTable.vue'
+
+const table = {
+    data: [{
+        id: 1,
+        name: 'Tamim'
+    }, {
+        id: 2,
+        name: 'Zubair'
+    }],
+    columns: [
+        { title: 'id', field: 'id' },
+        { title: 'Name', field: 'name' },
+    ]
+}
 </script>
 
 <template>
-    <NuxtLayout name="dashboard" title="Data Santri">
-        hexx
+    <NuxtLayout name="dashboard" title="Data Kelas">
+        <DataTable :table="table"></DataTable>
     </NuxtLayout>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/tbb-ui/src/components/layouts/*.vue",
+    "./node_modules/tbb-ui/src/components/**/*.vue",
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
