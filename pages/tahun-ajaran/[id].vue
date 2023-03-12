7ec806832796105519ea15329d0aec21f7a47b50
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-12 21:46:11 +0700

    improve UX

--- pages/tahun-ajaran/[id].vue
+++ pages/tahun-ajaran/[id].vue
@@ -2,8 +2,10 @@
 import AppCard from 'tbb-ui/src/components/AppCard.vue'
 import FormInput from 'tbb-ui/src/components/form/FormInput.vue'
 import AppButton from 'tbb-ui/src/components/button/AppButton.vue'
+import AppAlert from 'tbb-ui/src/components/AppAlert.vue'
 
 const { params } = useRoute()
+const appAlert = ref<InstanceType<typeof AppAlert> | null>(null)
 const form = reactive({
     name: ''
 })
@@ -17,11 +19,14 @@ try {
 
 const loading = ref(false)
 const save = async () => {
+    appAlert.value?.close()
+
     loading.value = true
     try {
         await $fetch(`/api/academic-year/${params.id}`, { method: 'PATCH', body: form })
+        appAlert.value?.open()
     } catch (error) {
-        alertError()
+        appAlert.value?.open('danger')
     }
     loading.value = false
 }
@@ -31,9 +36,11 @@ const save = async () => {
 <template>
     <NuxtLayout name="dashboard" :title="`${params.id === 'tambah' ? 'Tambah' : 'Edit'} Tahun Ajaran`">
         <AppCard>
+            <AppAlert ref="appAlert"></AppAlert>
             <FormInput v-model="form.name" label="Tahun Ajaran"></FormInput>
             <div class="text-right mt-5">
-                <AppButton @click="save"></AppButton>
+                <AppButton @click="useRouter().go(-1)" color="secondary" class="mr-2">Kembali</AppButton>
+                <AppButton @click="save" :loading="loading"></AppButton>
             </div>
         </AppCard>
     </NuxtLayout>
