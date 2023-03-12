c7bfdccfe3b5be08ba846e529d0f5f0076d3d8e4
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-12 17:54:33 +0700

    add basic input

---
+++ pages/tahun-ajaran/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AppCard from 'tbb-ui/src/components/AppCard.vue'
+import FormInput from 'tbb-ui/src/components/form/FormInput.vue'
+</script>
+
+
+<template>
+    <NuxtLayout name="dashboard" title="Tahun Ajaran - ">
+        <AppCard>
+            <FormInput label="Tahun Ajaran"></FormInput>
+        </AppCard>
+    </NuxtLayout>
+</template>
--- pages/tahun-ajaran/index.vue
+++ pages/tahun-ajaran/index.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
 import CrudDataTable from 'tbb-ui/src/components/table/CrudDataTable.vue'
 
-const loading = ref(false)
-const route = useRoute()
-
-const data = await $fetch('/api/academic-year')
 const table = {
     url: '/api/academic-year',
     columns: [
         { title: 'Name', field: 'name' },
         { title: 'Action', field: 'action', class: 'text-center w-32' },
     ]
-}
-
-const deleteItem = (id: string) => {
-
 }
 </script>
 
