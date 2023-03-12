4fa3cbe1e47019a018b18d38e666c6d0ce22cea8
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-12 10:01:27 +0700

    add ta page layout

--- composables/useMenus.ts
+++ composables/useMenus.ts
@@ -3,4 +3,5 @@ import IconHome from "tbb-ui/src/components/icons/IconHome.vue";
 export const useMenus = [
   { name: "Santri", path: "/santri", icon: IconHome },
   { name: "Kelas", path: "/kelas", icon: IconHome },
+  { name: "Tahun Ajaran", path: "/tahun-ajaran", icon: IconHome },
 ];
---
+++ pages/tahun-ajaran.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import DataTable from 'tbb-ui/src/components/table/DataTable.vue'
+
+const table = {
+    data: [{
+        id: 1,
+        name: 'Tamim'
+    }, {
+        id: 2,
+        name: 'Zubair'
+    }],
+    columns: [
+        { title: 'id', field: 'id' },
+        { title: 'Name', field: 'name' },
+    ]
+}
+</script>
+
+<template>
+    <NuxtLayout name="dashboard" title="Data Tahun Ajaran">
+        <DataTable :table="table"></DataTable>
+    </NuxtLayout>
+</template>
