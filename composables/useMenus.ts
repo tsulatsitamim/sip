caf473f63a16c48bd651e83da830e878802796b6
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-14 10:46:59 +0700

    revert menu name

--- composables/useMenus.ts
+++ composables/useMenus.ts
@@ -1,7 +1,7 @@
 import IconHome from "tbb-ui/src/components/icons/IconHome.vue";
 
 export const useMenus = [
-  { name: "Data Induk Santri", path: "/santri", icon: IconHome },
+  { name: "Santri", path: "/santri", icon: IconHome },
   { name: "Kelas", path: "/kelas", icon: IconHome },
   { name: "Tahun Ajaran", path: "/tahun-ajaran", icon: IconHome },
 ];
