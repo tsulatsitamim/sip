e4c65da1a5a19d48a89892ae067271c0f085c7f9
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2023-03-18 14:53:15 +0700

    add logout button

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -4,10 +4,14 @@ import AppDashboard from 'tbb-ui/src/components/layouts/AppDashboard.vue'
 defineProps({
     title: { type: String, default: '' }
 })
+
+const logout = () => {
+    useLogout();
+}
 </script>
 
 <template>
-    <AppDashboard :title="title" :settings="useSettings" :menus="useMenus">
+    <AppDashboard :title="title" :settings="useSettings" :menus="useMenus" @logout="logout">
         <template #toolbar>
             <slot name="toolbar"></slot>
         </template>
